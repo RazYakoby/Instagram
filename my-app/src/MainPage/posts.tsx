55722543d485991cd8ff398b9f9ba84236e7c53b
RazYakoby <razrazraz189@gmail.com>
2024-04-26 15:00:46 +0300

    fix the counter

--- my-app/src/MainPage/posts.tsx
+++ my-app/src/MainPage/posts.tsx
@@ -2,78 +2,71 @@ import heartIcon from '../heart-icon.jpg';
 import heartRedIcom from '../heartRed-icon.jpg';
 import '../cssFile/posts.css';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 
 function Posts(){
 
     const images = [heartIcon, heartRedIcom];
-    const images2 = useParams();
-    const [imageUrl, setImageUrl] = useState<string>('');
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const [count, setCount] = useState( () => {
+    const [currentImageIndex, setCurrentImagesIndex] = useState(0);
+
+    const handleClick = () => {
+        setCurrentImagesIndex((currentImageIndex + 1) % images.length);
+    }
+
+    const [count, setCount] = useState(() => {
         const saveCount = localStorage.getItem('likeCount');
         return saveCount ? parseInt(saveCount, 10) : 0;
     });
 
-    const handleClick = (count:number) => {
-      setCurrentImageIndex((count + 1) % images.length);
-    };
-
     useEffect(() => {
-        const storedImageUrl = localStorage.getItem('imageUrl');
-        if (storedImageUrl) {
-            setImageUrl(storedImageUrl);
-        }
-        const like = document.getElementById('like');
-        const handleClick = () => {
-    
-          if (images[currentImageIndex] === heartRedIcom) {
-            setCount((prevCount) => {
-              const newCount = prevCount - 1;
-              localStorage.setItem('likeCount', newCount.toString());
-              return newCount;
-            });
-          }
-            else{
-                setCount((prevCount) => {
-                  const newCount = prevCount + 1;
-                  localStorage.setItem('likeCount', newCount.toString());
-                  return newCount;
-                });
-              }
-            };
-        
-            if(like){
-            like.addEventListener('click', handleClick);
-        
-            return () => {
-              like.removeEventListener('click', handleClick);
-            };
-        }
+        const like1 = document.getElementById('like1');
+        const like2 = document.getElementById('like2');
 
-          }, [currentImageIndex, images]);
+        const handleClick1 = () => {
+            if (images[currentImageIndex] === heartRedIcom) {
+                setCount((prev) => {
+                    const newCount = prev - 1;
+                    localStorage.setItem('likeCount', newCount.toString());
+                    return newCount;
+                })
+            } else {
+                setCount((prev) => {
+                    const newCount = prev + 1;
+                    localStorage.setItem("likeCount", newCount.toString());
+                    return newCount;
+                })
+            }
+        };
 
+        like1?.addEventListener('click', handleClick1);
+        like2?.addEventListener('click', handleClick1);
+
+        return () => {
+            like1?.removeEventListener('click', handleClick1);
+            like2?.removeEventListener('click', handleClick1);
+        }
+    }, [currentImageIndex, images]);
 
     return(
         <>
             <nav className="post">
-                <img src={heartIcon} className="img"></img>
+                <img src={heartIcon} className="img" alt='heartIcon'></img>
                 <div className='features'>
                     <button>comments</button>
-                    <button name='like' onClick={ () => handleClick(currentImageIndex)}>
+                    <button id='like1' name='like' onClick={handleClick}>
                         <img src={images[currentImageIndex]} alt='image' className='likes'/>
                     </button>
+                    <h4>{count}</h4>
                 </div>
             </nav>
 
             <nav className="post">
-                <img src={heartRedIcom} className="img"></img>
+                <img src={heartRedIcom} className="img" alt='heartRedIcon'></img>
                 <div className='features'>
                     <button>comments</button>
-                    <button name='like' onClick={ () => handleClick(currentImageIndex)}>
+                    <button id='like2' onClick={handleClick}>
                         <img src={images[currentImageIndex]} alt='image' className='likes'/>
-                        <text>{count}</text>
                     </button>
+                    <h4>{count}</h4>
                 </div>
             </nav>
         </>
